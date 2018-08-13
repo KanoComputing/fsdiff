16c0ffda3da387baef144ee9f88af3d4afb53bb4
Ioannis Valasakis <code@wizofe.uk>
2018-08-13 01:23:26 +0000

    Add colorama module and update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
+#!/usr/bin/env python3
+
 # Copyright (C) 2018 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 
 
 from setuptools import setup
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name='fsdiff',
-    version='0.1',
+    version='0.2.post3',
     description='Byte to byte comparison of local disk images or filesystems',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     author='Team Kano',
     author_email='dev@kano.me',
     url='https://github.com/KanoComputing/fsdiff',
     packages=['fsdiff'],
-    scripts=['bin/fsdiff']
+    scripts=['bin/fsdiff'],
+    install_requires=[
+        "colorama"
+    ]
 )
