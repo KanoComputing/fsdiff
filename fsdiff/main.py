eb62da7553f3d14b660b1a1e3bd98b9f2286c708
Ioannis Valasakis <code@wizofe.uk>
2018-08-17 08:29:56 +0000

    Copyright

--- fsdiff/main.py
+++ fsdiff/main.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 Ioannis Valasakis <code@wizofe.uk>
+# Copyright (C) 2018 Kano Computing <dev@kano.me>
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 
 import os
